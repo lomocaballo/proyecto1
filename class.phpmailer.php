36649f52bbfb42169f7061ae3a67cf116f5b8aa2
Unknown <sete391@hotmail.com>
2017-11-30 19:19:50 +0100

    Cambio David

--- class.phpmailer.php
+++ class.phpmailer.php
@@ -1,5 +1,5 @@
 <?php
-/*~ class.phpmailer.php
+/*~ class.phpmailer.php sss
 .---------------------------------------------------------------------------.
 |  Software: PHPMailer - PHP email class                                    |
 |   Version: 5.2.1                                                          |
